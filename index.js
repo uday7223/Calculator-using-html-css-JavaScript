9ffb880a6a2348a9429719c3a5660ceac2919117
udaydevanur <uday.devnur223@gmail.com>
2024-05-13 02:08:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const display = document.getElementById("display");
+function appendToDisplay(input){
+    display.value += input;
+
+}
+
+function clearDisplay(){
+    display.value = "";
+
+}
+
+function calculate(){
+    display.value =eval(display.value);
+
+}
+
+function removeEle(){
+    display.value = display.value.slice(0,-1);
+}
